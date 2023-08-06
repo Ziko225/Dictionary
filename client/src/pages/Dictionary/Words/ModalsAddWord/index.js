a10c078d5acf63d0e079ab305bc3dd42351714ca
Nazar <nazar@bychyk.it>
2023-08-06 17:26:21 +0200

    Added focus and fixed styles in ModalsAddWord

--- client/src/pages/Dictionary/Words/ModalsAddWord/index.js
+++ client/src/pages/Dictionary/Words/ModalsAddWord/index.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const ModalsAddWord = ({ addWord }) => {
     const [name, setName] = useState("");
     const [translate, setTranslate] = useState("");
     const [msg, setMsg] = useState("");
+    const ref = useRef(null);
 
     const data = {
         name,
@@ -23,13 +24,14 @@ const ModalsAddWord = ({ addWord }) => {
 
         setName("");
         setTranslate("");
+        ref.current.focus();
     };
 
     return (
         <form onSubmit={(e) => addNewWorld(e)} className='addWord'>
             <span className='error'>{msg}</span>
-            <input value={name} onChange={(e) => setName(e.currentTarget.value)} required className='addWord__input' placeholder='Word'></input>
-            <input value={translate} onChange={(e) => setTranslate(e.currentTarget.value)} required className='addWord__input' placeholder='Translate'></input>
+            <input autoFocus ref={ref} value={name} onChange={(e) => setName(e.currentTarget.value)} required className='addWord__input' placeholder='Word' />
+            <input value={translate} onChange={(e) => setTranslate(e.currentTarget.value)} required className='addWord__input' placeholder='Translate' />
             <button className='addWord__button'>Add</button>
         </form>
     );
